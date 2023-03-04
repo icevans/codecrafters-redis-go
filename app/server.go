3b4072c9b96db6948f8481291e91e5d74d3a7776
Ian Evans <icevans@icloud.com>
2023-03-04 10:21:27 -0800

    feat: respond with PONG

--- app/server.go
+++ app/server.go
@@ -6,15 +6,36 @@ import (
 	"os"
 )
 
+func RedisSimpleString(s string) string {
+	return fmt.Sprintf("+%s\r\n", s)
+}
+
+func RedisError(kind string, msg string) string {
+	return fmt.Sprintf("-%s %s\r\n", kind, msg)
+}
+
+func RedisInteger(i int) string {
+	return fmt.Sprintf(":%d\r\n", i)
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer c.Close()
+
+	for {
+		r := make([]byte, 256)
+		c.Read(r)
+		fmt.Println(string(r))
+		c.Write([]byte(RedisSimpleString("PONG")))
 	}
 }
